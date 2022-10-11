0ae81d40ba538ed20bfaf2cc702c19cfa89aa750
Fernando Cavanellas <fernandocavanellas6@hotmail.com>
2022-10-11 18:23:49 -0300

    config weather

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ const tempElement = document.querySelector('#temperature span');
 const descElement = document.querySelector('#description');
 const weatherIconElement = document.querySelector('#weather-icon');
 const countryElement = document.querySelector('#country');
-const umidityElement = document.querySelector('#umidity span');
+const humidityElement = document.querySelector('#humidity span');
 const windElement = document.querySelector('#wind span');
 
 const getWeatherData = async city => {
@@ -18,11 +18,22 @@ const getWeatherData = async city => {
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
-    console.log(data);
+    return data;
 };
 
-const showWeatherData = city => {
-    getWeatherData(city);
+const showWeatherData = async city => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        'src',
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+    );
+    countryElement.setAttribute('src', apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
 };
 
 searchBtn.addEventListener('click', e => {
